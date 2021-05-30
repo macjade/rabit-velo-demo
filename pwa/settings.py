de2d4395ef8df0399ed8755c96507ed5d65d3a09
macjade <jamesdaniel00700@gmail.com>
2021-05-30 19:34:41 +0100

    fourth commit

--- pwa/settings.py
+++ pwa/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
+#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -82,25 +82,25 @@ WSGI_APPLICATION = 'pwa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'velodb',
-#         'USER': 'velodb',
-#         'PASSWORD': 'Ok9C-kiJ7NN?',
-#         'HOST': 'den1.mysql2.gear.host',
-#     },
-#     'OPTIONS': {
-#         'sql_mode': 'traditional',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'velodb',
+        'USER': 'velodb',
+        'PASSWORD': 'Ok9C-kiJ7NN?',
+        'HOST': 'den1.mysql2.gear.host',
+    },
+    'OPTIONS': {
+        'sql_mode': 'traditional',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
